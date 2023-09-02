e64711b8c6e4d86806b0286da40f8d08340d1a93
luanrem <luanrem@gmail.com>
2023-09-02 13:06:34 -0300

    refactor: fix eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
     <div className="m-2 flex flex-col">
-      <h1 className="m-3 mx-1 font-baloo text-3xl text-black">Hello World</h1>
+      <h1 className="m-3 mx-1 font-baloo text-3xl text-black">
+        Hello World yhis sfd
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
