a9528060a44994e4535a0f7153b45d5a3190af1c
luanrem <luanrem@gmail.com>
2023-09-02 00:22:55 -0300

    feat: configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
-    <>
-      <h1 className='text-black font-baloo text-3xl'>Hello World</h1>
-    </>
-  )
+    <div className="m-2 flex flex-col">
+      <h1 className="m-3 mx-1 font-baloo text-3xl text-black">Hello World</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
