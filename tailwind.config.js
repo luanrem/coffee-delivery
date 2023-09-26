0487a49ed5268905053e26e67b907051d923c728
luanrem <luanrem@gmail.com>
2023-09-25 21:43:33 -0300

    feat: install icon library and config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'roboto': ['Roboto', 'sans-serif'],
-        'baloo': ['"Baloo 2"', 'cursive'],
-      }
+        roboto: ['Roboto', 'sans-serif'],
+        baloo: ['"Baloo 2"', 'cursive'],
+      },
+      colors: {
+        purple: 'rgba(var(--purple))',
+        'purple-dark': 'rgba(var(--purple-dark))',
+        'purple-light': 'rgba(var(--purple-light))',
+      },
     },
   },
   plugins: [],
 }
-
