238b19af04478cdbdac4f42cef903239632c262f
luanrem <luanrem@gmail.com>
2023-09-28 20:59:31 -0300

    feat: add Checkout routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
+import Home from './pages/Home'
 import Header from './Layout/Header'
+import Checkout from './pages/Checkout'
 
 export function Router() {
   return (
     <Routes>
       <Route element={<Header />}>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Route>
     </Routes>
   )
