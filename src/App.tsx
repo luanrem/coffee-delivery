5db2ac1f14911cf7aa234ae2567a6131397dd5f0
luanrem <luanrem@gmail.com>
2023-09-30 18:33:38 -0300

    feat: install react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import { queryClient } from './api/queryClient'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
