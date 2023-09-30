ca4175625fcf2b18bbd19d4af27eb4867b62c39f
luanrem <luanrem@gmail.com>
2023-09-30 18:25:30 -0300

    feat: install react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import { queryClient } from './api/queryClient'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
