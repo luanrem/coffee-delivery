09e868adb4173c999ffe2408a8a1af98a109b541
luanrem <luanrem@gmail.com>
2023-10-04 21:01:04 -0300

    feat: create checkout cards

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,3 +1,34 @@
 export default function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <div className="flex flex-wrap gap-8 px-32">
+      <section className="w-160 flex flex-col gap-4">
+        <h2 className="font-baloo text-lg">Complete seu pedido</h2>
+        <div className="w-full rounded-md rounded-bl-[2.25rem] rounded-tr-[2.25rem] bg-base-card p-10">
+          <p>Endereço de Entrega</p>
+        </div>
+      </section>
+      <section className="w-112 flex flex-col gap-4">
+        <h2 className="font-baloo text-lg">Cafés selecionados</h2>
+        <div className="flex w-full flex-col gap-6 rounded-md rounded-bl-[2.25rem] rounded-tr-[2.25rem] bg-base-card p-10">
+          <div className="flex flex-col gap-3">
+            <div className="flex justify-between">
+              <p>Total de itens</p>
+              <p>R$ 29,70</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="font-roboto">Entrega</p>
+              <p>R$ 3,50</p>
+            </div>
+            <div className="flex justify-between">
+              <p>Total</p>
+              <p>R$ 33,20</p>
+            </div>
+          </div>
+          <button className="h-12 rounded-md bg-yellow font-roboto text-sm font-bold text-white">
+            CONFIRMAR PEDIDO
+          </button>
+        </div>
+      </section>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      baloo: ['"Baloo 2"', 'cursive'],
+    },
     extend: {
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
-        baloo: ['"Baloo 2"', 'cursive'],
       },
       colors: {
         'purple-dark': '#4B2995',
@@ -23,14 +25,18 @@ export default {
       },
       fontSize: {
         '2xs': '0.625rem',
-        //       font-size: 0.75rem/* 12px */;
-        // line-height: 1rem/* 16px */;
       },
       lineHeight: {
         '2xs': '1rem',
       },
       backgroundImage: {
         'home-section': "url('/src/assets/home-background.png')",
+      },
+      width: {
+        112: '28rem',
+        128: '32rem',
+        144: '36rem',
+        160: '40rem',
       },
     },
   },
