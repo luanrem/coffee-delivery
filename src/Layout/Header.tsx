5403c192053ebdd784496963092db0dd01074e2a
luanrem <luanrem@gmail.com>
2023-10-07 20:51:26 -0300

    feat: make header responsible

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -6,14 +6,14 @@ import { HiMiniMapPin } from 'react-icons/hi2'
 export default function Header() {
   return (
     <>
-      <header className="flex h-24 items-center justify-between px-32">
+      <header className="flex h-24 items-center justify-between px-5 lg:px-32">
         <Link to={'/'}>
           <CoffeeLogo />
         </Link>
         <div className="flex gap-3">
           <button className="flex h-9 w-auto items-center gap-1 rounded-md bg-purple-light px-2 py-2 font-roboto transition-colors hover:bg-purple-light/70">
             <HiMiniMapPin className="text-2xl text-purple" />
-            <p className="text-purple-dark">Porto Alegre, RS</p>
+            <p className=" text-purple-dark ">Porto Alegre, RS</p>
           </button>
           <button className="relative flex h-9 w-9 items-center justify-center rounded-md bg-yellow-light transition-colors hover:bg-yellow-light/70">
             <CheckoutCar />
