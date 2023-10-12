3d2bc046c0dcb4677dff1f0e2f20bebc8cde2db5
luanrem <luanrem@gmail.com>
2023-10-12 14:23:55 -0300

    feat: create formatter utils

---
+++ src/utils/formatter.ts
@@ -0,0 +1,9 @@
+export const BRFormatter = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+})
+
+export const USFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
